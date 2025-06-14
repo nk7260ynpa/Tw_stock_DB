42f5267734230590729dfe7c337b2b622a3ba403
Chen <nk7260ynpa@gmail.com>
2025-06-14 23:11:25 +0800

    Update daily crawlers to use dynamic date for uploads and adjust scheduling times

--- DailyUpload.py
+++ DailyUpload.py
@@ -21,7 +21,7 @@ def twse_daily_craw():
                     "password": PASSWORD, 
                     "dbname": DBNAME, 
                     "crawlerhost": CRAWLERHOST})
-    upload.day_upload("2024-06-14", opt)
+    upload.day_upload(DATE, opt)
 
 def tpex_daily_craw():
     """
@@ -38,12 +38,12 @@ def tpex_daily_craw():
                     "password": PASSWORD, 
                     "dbname": DBNAME, 
                     "crawlerhost": CRAWLERHOST})
-    upload.day_upload("2024-06-14", opt)
+    upload.day_upload(DATE, opt)
 
 
 if __name__ == "__main__":
-    schedule.every().day.at("18:40").do(twse_daily_craw)
-    schedule.every().day.at("18:48").do(tpex_daily_craw)
+    schedule.every().day.at("16:13").do(twse_daily_craw)
+    schedule.every().day.at("16:18").do(tpex_daily_craw)
 
     while True:
         schedule.run_pending()
