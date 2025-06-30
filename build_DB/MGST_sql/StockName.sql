bfa91bfb022f1d16838ceacefac9983ed5303b7e
Chen <nk7260ynpa@gmail.com>
2025-06-30 09:44:26 +0800

    Add BuildMGTSTABLEStockName class for uploading StockName data from CSV

---
+++ build_DB/MGST_sql/StockName.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `MGST`.`StockName` (
+    `SecurityCode` VARCHAR(10) NOT NULL,
+    `StockName` VARCHAR(15) NOT NULL,
+    PRIMARY KEY (`SecurityCode`)
+)
--- build_DB/mgts.py
+++ build_DB/mgts.py
@@ -22,3 +22,18 @@ class BuildMGTSTABLEDailyPrice(BuildMGTSTABLE):
     
     def post_process(self, conn):
         pass
+
+class BuildMGTSTABLEStockName(BuildMGTSTABLE):
+    def __init__(self):
+        super().__init__()
+    
+    def post_process(self, conn):
+        """
+        Upload the Security Code data match StockName table.
+
+        Args:
+            conn: Database connection object.
+        """
+        df = pd.read_csv("build_DB/MGTS_sql/mgts_code.csv")
+        df.to_sql("StockName", conn, if_exists='append', index=False, chunksize=1000)
+        conn.commit()
