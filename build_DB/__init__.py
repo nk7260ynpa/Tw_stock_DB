89d55f374b1a22db542c887c768e0ebfd2d276cc
Chen <nk7260ynpa@gmail.com>
2025-06-30 09:55:55 +0800

    Add MGTS imports for database processing in __init__.py

--- build_DB/__init__.py
+++ build_DB/__init__.py
@@ -3,3 +3,4 @@ from .twse import BuildTWSE, BuildTWSETABLE, BuildTWSETABLEDailyPrice
 from .tpex import BuildTPEX, BuildTPEXTABLE, BuildTPEXTABLEDailyPrice
 from .taifex import BuildTAIFEX, BuildTAIFEXTABLE, BuildTAIFEXTABLEDailyPrice
 from .faoi import BuildFAOI, BuildFAOITABLE, BuildFAOITABLEDailyPrice
+from .mgts import BuildMGTS, BuildMGTSTABLE, BuildMGTSTABLEDailyPrice
